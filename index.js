20c92d4cc69dd21f5e804f7c8626fcb6b306172d
singhArmani <singh_aman@hotmail.com.au>
2020-08-07 15:16:24 +1000

    Use cache result before calling recursively

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const fs = require("fs");
 fs.readFile("things.csv", "utf8", function (err, data) {
   const dataArray = data.split(/\r?\n/).map((r) => r.split(","));
 
-  const outputArray = dataArray.map((row) =>
-    row.map((exp) => evaluatePostFix(exp, dataArray))
-  );
-
   //const dummyData = [
-  //["1 B1 +", "2 B2 *"],
-  //["5", "78"],
+  //["B2 B1 +", "2 B2 *", "A1"],
+  //["A1", "78"],
+  //["B1", "B2"],
   //];
-  //const outputArray = dummyData.map((row) =>
-  //row.map((exp) => evaluatePostFix(exp, dummyData))
-  //);
 
-  console.log(outputArray);
+  let resultArray = [];
+  for (let i = 0; i < dataArray.length; i++) {
+    resultArray[i] = [];
+    for (let j = 0; j < dataArray[i].length; j++) {
+      const result = evaluatePostFix(dataArray[i][j], dataArray, resultArray);
+      resultArray[i][j] = result;
+    }
+  }
+
+  // TODO: write it to a file if we want
+  console.log(resultArray);
 });
 
 const operationMap = {
@@ -29,15 +33,13 @@ const operationMap = {
 };
 
 // expression: 'B1 B2 +'
-function evaluatePostFix(expression, dataArray) {
-  console.log("starting with: ", { expression });
+function evaluatePostFix(expression, dataArray, resultArray) {
   const stack = [];
   // split the string to array
   // ['B1', 'B2', '+']
   const tokenArray = expression.split(" ");
 
   for (let i = 0; i < tokenArray.length; i++) {
-    console.log("for loop...", { stack });
     let token = tokenArray[i];
     if (isOperand(token)) {
       // If it's an operand, keep collecting it into the stack
@@ -46,8 +48,6 @@ function evaluatePostFix(expression, dataArray) {
       // let's try to pop our operands, and perform operation on them
       const op2 = stack.pop();
       const op1 = stack.pop();
-
-      console.log({ token, op1, op2 });
 
       if (!(Boolean(op1) && Boolean(op2))) {
         // We don't have any Number type to operate on
@@ -69,28 +69,29 @@ function evaluatePostFix(expression, dataArray) {
         groups: { col, row },
       } = /(?<col>^[A-Z])(?<row>\d$)/.exec(token);
 
-      console.log("match...", { col, row });
       const colIdx = getIdxFromChar(col);
-      console.log("index rep", { colIdx });
 
-      //console.log({ dataArray });
       // recursive call this evaluatePostFix and try to get the value using <col><row>
       const exp = dataArray[row - 1][colIdx];
 
-      console.log({ exp });
+      // before calling recursively, let's check if we have stored that into our result array
+      let recursiveResult;
+      if (Array.isArray(resultArray[row - 1]) && resultArray[row - 1][colIdx]) {
+        console.log("found it...", { token }, resultArray[row - 1][colIdx]);
+        recursiveResult = resultArray[row - 1][colIdx];
+      } else {
+        recursiveResult = evaluatePostFix(exp, dataArray, resultArray);
+      }
 
-      const recursive = evaluatePostFix(exp, dataArray);
-      console.log({ recursive });
-      stack.push(recursive);
+      stack.push(recursiveResult);
     } else {
       return "#ERR";
     }
   }
 
-  // missing operator: '1 2'
+  // missing operator: '1 2'; '1' is valid though
   if (stack.length >= 2) return "#ERR";
 
-  console.log("stack...............", { stack });
   // return the top of stac
   return stack[stack.length - 1];
 }
